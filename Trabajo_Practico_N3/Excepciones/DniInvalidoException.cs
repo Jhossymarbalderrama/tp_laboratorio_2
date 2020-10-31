79d0c287b6bb1a51857332e2e4e0ab64d57dd441
JhossyB <jhossymarbalderrama@gmail.com>
2020-10-31 13:26:12 -0300

    TP3

---
+++ Trabajo_Practico_N3/Excepciones/DniInvalidoException.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excepciones
+{
+    public class DniInvalidoException : Exception
+    {
+        public DniInvalidoException():base("DNI con error de Formato")
+        {
+
+        }
+        public DniInvalidoException(Exception e)
+            : base("DNI con error de Formato :"+ e.Message)
+        {
+
+        }
+        public DniInvalidoException(string message)
+            : base(message)
+        {
+
+        }
+        public DniInvalidoException(string message,Exception e)
+            : base(message,e)
+        {
+
+        }
+    }
+}
